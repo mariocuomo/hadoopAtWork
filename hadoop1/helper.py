17955224a6d23c9c332bd4168215bca777b9d8fc
mariocuomo <cuomomario@hotmail.com>
2022-03-24 16:14:57 +0100

    update helper

--- hadoop1/helper.py
+++ hadoop1/helper.py
@@ -4,10 +4,27 @@ import random
 
 alphabet = list(string.ascii_lowercase)
 
-with open('file.txt', 'w') as out:
+
+
+
+"""
+genera un file che ha la forma
+CARATTERE | NUMERO INTERO 
+"""
+with open('fileA.txt', 'w') as out:
 	for i in range(100000):
 		letter=alphabet[random.randint(0, len(alphabet)-1)]
 		number=str(random.randint(0, 100))
 
-		out.write(letter +' '+ number+'\n')
+		out.write(letter + ' ' + number+'\n')
+
+
+"""
+genera un file che ha la forma
+CARATTERE | CODICE ALFANUMERICO 
+"""
+with open('fileB.txt', 'w') as out:
+	for carattere in alphabet:
+		codice = str(random.randint(0, 100))+carattere+str(random.randint(0, 100))
+		out.write(carattere + ' ' + codice+'\n')
 
